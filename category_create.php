224c991df55512e4a7eaaa0ab6751a3c24665078
Solo <shineboboaung00@gmail.com>
2026-07-25 20:41:40 +0630

    category create page (Character Checking System) (25.7.26)

--- category_create.php
+++ category_create.php
@@ -1,19 +1,32 @@
-<?php 
+<?php
 include 'components/connection.php';
+
+$error = '';
 
 if (isset($_POST['submit'])) {
     $name = mysqli_real_escape_string($connection, $_POST['name']);
     $image = $_FILES['image']['name'];
 
-    $data = "INSERT INTO category (name,image) VALUES ('$name','$image')";
-
-    if (!empty($image)) {
-        move_uploaded_file($_FILES['image']['tmp_name'], 'static/' .$image);
+    if (empty($name)) {
+        $error = "Name is required";
     }
+    else if (strlen($name) < 5 || strlen($name) > 15) {
+        $error = "Name must be between 5 and 15 characters long.";
+    } 
+    else {
+        if (!empty($image)) {
+            move_uploaded_file($_FILES['image']['tmp_name'], 'static/' . $image);
+        }
+        $data = "INSERT INTO category (name,image) VALUES ('$name','$image')";
 
-    $connection->query($data);
-    header('location: shop.php');
-    exit();
+        if ($connection->query($data)) {
+            header('Location: shop.php');
+            exit();
+        }
+        else {
+            $error = "Database Error: " . $connection->error;
+        }
+    }
 }
 
 
@@ -39,17 +52,21 @@ if (isset($_POST['submit'])) {
 
     <!-- register form start  -->
 
-    <div class="register-form" >
+    <div class="register-form">
 
         <form method="POST" enctype="multipart/form-data">
             <h3>Category Create Form</h3>
+
+            <?php if (!empty($error)): ?>
+                <p class="error_warning"><?php echo $error; ?></p>
+            <?php endif; ?>
 
             <input type="text" name="name" placeholder="Enter your category name" class="box" required>
             <input type="file" name="image" class="box" required>
             <button type="submit" name="submit" class="btn">Create Now</button>
 
         </form>
-        
+
     </div>
 
     <!-- register form end  -->
